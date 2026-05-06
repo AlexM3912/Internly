457c54b31c3982e21a16148a0d35682c8ea6c5d3
AlexM3912 <alextmanov@gmail.com>
2026-05-06 08:37:11 -0500

    firsst commit

---
+++ Internly-AMAC/Map.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var cameraPosition: MapCameraPosition = .region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 42.0883603, longitude: -87.9806265),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+    )
+
+    var body: some View {
+        ZStack {
+            Map(position: $cameraPosition)
+                .ignoresSafeArea()
+
+            Text("Internly")
+                .font(.system(size: 50, weight: .semibold))
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [.white, .purple, .black],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+
+        }
+    }
+}
+//create it so from the adress we create the cordinates and then they get positioned on the map
+
