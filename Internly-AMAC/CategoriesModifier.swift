7d8d891e030f3d0fa27c4e05899fb5260526fb5c
AlexM3912 <alextmanov@gmail.com>
2026-05-14 14:57:07 -0500

    now you can select a category. without lag

---
+++ Internly-AMAC/CategoriesModifier.swift
@@ -0,0 +1,36 @@
+//
+//  frameForCategories.swift
+//  Internly-AMAC
+//
+//  Created by Alexander T. Manov on 5/14/26.
+//
+
+
+//
+//  PersonalizationModifier.swift
+//  Internly-AMAC
+//
+//  Created by Alexander T. Manov on 5/12/26.
+//
+
+import SwiftUI
+
+struct frameForCategories: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .frame(width: 200, height: 100, alignment: .center)
+            .background(
+                RadialGradient(colors: [.white, .green], center: .top, startRadius: 5, endRadius: 150)
+            )
+            .foregroundStyle(
+                LinearGradient(
+                    colors: [.yellow, .green, .yellow],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+            .border(Color.green, width: 2)
+            .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
+}
